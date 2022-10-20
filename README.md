8869956df9ca3178c49dbc852bbc3e636f2a6827
Tea.Lin <gjlmotea@gmail.com>
2022-10-20 14:45:02 +0800

    update script and img

--- README.md
+++ README.md
@@ -1,18 +1,26 @@
 # cmcVoteBot
 
-A vote bot script in [CoinMarketCap site](https://coinmarketcap.com/) !!  
+A vote bot script that everyone can run in computer.    
 
 
+## Vote the question
+[CoinMarketCap Site](https://coinmarketcap.com/)
 > How do you feel about USD Coin today?  
 > Vote to see community results  
 
 **Let's vote [MEWC](https://coinmarketcap.com/zh-tw/currencies/meowcoin/) to 100% Good**    
 
+![img_1.png](img_1.png)
+
+## Download
+
+    > git clone https://github.com/gjlmotea/cmcVoteBot
+
 ## Install
 
 Please install [node.js](https://nodejs.org/en/download/) first.  
 
-    > npm i axios
+    > npm install
 
 ## Run with node.js
 
@@ -36,4 +44,5 @@ Let's promote currency circulation and make $MEWC more valuable.
 After received, **I will send 50% of them to Burn Address**, to reduce the total money supply.      
 * [MeowCoin Burn](https://explorer.mewccrypto.com/address/MCBurnXXXXXXXXXXXXXXXXXXXXXXUkdzqy) address: `MCBurnXXXXXXXXXXXXXXXXXXXXXXUkdzqy`
 
+> $MEWC to the moon
 
--- main.js
+++ main.js
@@ -1,42 +1,51 @@
-const coinCode = 22253;
-const interval = 7000; // no less than 5000 ms
+const interval = 3000; // no less than 3000 ms
 const count = 10000; // number of votes
+const coinCode = 22253; // $MEWC code
+
 const axios = require('axios');
 const url = 'https://api.coinmarketcap.com/data-api/v3/cryptocurrency/vote';
 
-let vote = function () {
-    axios.get(url + '?id=' + coinCode)
+const getVotedId = async function () {
+    return await axios.get(url + '?id=' + coinCode)
         .then((response) => {
-            let votedId = response.data['data']['unregisteredId']
+            return response.data['data']['unregisteredId'];
+        })
+        .catch(reason => {
+            // console.warn(reason['response']['data']);
+        });
+};
 
-            axios.post(url, JSON.stringify({
-                'cryptoId': coinCode,
-                'voted': 1, // 1: good, 2: bad
-                'votedId': votedId,
-            }), {
-                headers: {
-                    'Content-Type': 'application/json'
-                }
-            })
-                .then((response) => {
-                    console.log(votedId, ':', response.data['status']['error_message']);
-                })
-                .catch(reason => {
-                    console.warn(reason['response']['data']);
-                })
+const vote = async function (votedId) {
+    console.log('(do vote...)')
+    return await axios.post(url, JSON.stringify({
+        'cryptoId': coinCode,
+        'voted': 1, // 1: good, 2: bad
+        'votedId': votedId,
+    }), {
+        headers: {
+            'Content-Type': 'application/json'
+        }
+    })
+        .then((response) => {
+            console.log(votedId, ':', response.data['status']['error_message']);
         })
         .catch(reason => {
             console.warn(reason['response']['data']);
         })
-
 };
-
 
 (async () => {
     for (let i = 0; i < count; i++) {
-        vote();
-        await new Promise(resolve => setTimeout(resolve, interval));
         console.log('==========')
+
+        const votedId = await getVotedId();
+        await new Promise(resolve => setTimeout(resolve, interval / 2));
+        if (votedId) {
+            await vote(votedId);
+        } else {
+            console.warn('(too freq...)')
+        }
+        await new Promise(resolve => setTimeout(resolve, interval / 2));
     }
 })();
 
